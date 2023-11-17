3ba935c4a7882fbf9430a213db779dccaffe0b63
GermanBalaguera954 <115397696+GermanBalaguera954@users.noreply.github.com>
2023-11-17 12:51:56 -0500

    Add files via upload

---
+++ base.php
@@ -0,0 +1,26 @@
+<?php
+require 'includes/funciones.php';
+incluirTemplate('header');
+?>
+
+  <main class="contenedor seccion">
+    <h1>Titulo Página</h1>
+  </main>
+
+  <footer class="footer seccion">
+    <div class="contenedor contenedor-footer">
+      <nav class="navegacion">
+        <a href="nosotros.html">Nosotros</a>
+        <a href="anuncios.html">Anuncios</a>
+        <a href="blog.html">Blog</a>
+        <a href="contacto.html">Contacto</a>
+      </nav>
+    </div>
+
+    <p class="copyright">Todos los derechos Reservados 2023 &copy;</p>
+  </footer>
+
+  <script src="build/js/bundle.min.js"></script>
+</body>
+
+</html>
